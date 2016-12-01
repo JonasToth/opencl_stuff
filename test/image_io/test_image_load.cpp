2055adead93f5e932d8c310fd2bf030073b40432
Jonas Toth <jonas.toth@gmail.com>
2016-12-01 23:09:23 +0100

    [Test] Implement imageload test with google test

--- test/image_io/test_image_load.cpp
+++ test/image_io/test_image_load.cpp
@@ -1,51 +1,27 @@
-#include "image_io/Image.h"
+#include <gtest/gtest.h>
 #include <iostream>
 
-/// testload of an jpg
-int main()
+#include "image_io/Image.h"
+
+
+TEST(LoadImage, Constructor)
 {
-    // everything alright for the moment
-    int error_code = 0;
+    auto img = Image("test_load.jpg");
+    EXPECT_EQ(img.width(), 172);
+    EXPECT_EQ(img.height(), 178);
+}
 
-    // ======================== Load over constructor ====================================
-    try {
-        auto img = Image("test_load.jpg");
-        std::cout << "Success from constructor!" << std::endl;
+TEST(LoadImage, Method)
+{
+    auto img = Image();
+    img.load("test_load.jpg");
+    EXPECT_EQ(img.width(), 172);
+    EXPECT_EQ(img.height(), 178);
+}
 
-        if(img.width() != 172) {
-            std::cout << "Width is wrong" << std::endl;
-            error_code = 1;
-        }
-        if(img.height() != 178) {
-            std::cout << "Height is wrong" << std::endl;
-            error_code = 1;
-        }
-    } catch(std::runtime_error& e) {
-        std::cout << "Failed to load image!" << std::endl;
-        std::cout << e.what() << std::endl;
-        error_code = 1;
-    }
-
-
-    // ====================== Load with member function ==================================
-    try {
-        auto img = Image();
-        img.load("test_load.jpg");
-        std::cout << "Success from member function!" << std::endl;
-
-        if(img.width() != 172) {
-            std::cout << "Width is wrong" << std::endl;
-            error_code = 1;
-        }
-        if(img.height() != 178) {
-            std::cout << "Height is wrong" << std::endl;
-            error_code = 1;
-        }
-    } catch(std::runtime_error& e) {
-        std::cout << "Failed to load image over member function!" << std::endl;
-        std::cout << e.what() << std::endl;
-        error_code = 1;
-    }
-
-    return error_code;
+/// testload of an jpg
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
