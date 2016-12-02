a94083c2fa4cba4380135513f1485ca68c105f55
Jonas Toth <jonas.toth@gmail.com>
2016-12-02 22:15:00 +0100

    [Test] CL info test improved

--- test/color_conversion/test_cl_info.cpp
+++ test/color_conversion/test_cl_info.cpp
@@ -1,10 +1,57 @@
 #include <CL/cl.hpp>
+#include <gtest/gtest.h>
 #include <iostream>
 #include <vector>
 
 
-int main() { 
+TEST(OpenClInfo, BasicFunctionality)
+{
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
-    return 0; 
+
+    EXPECT_TRUE(platforms.size() > 0);
+}
+
+TEST(OpenClInfo, DefaultPlatform)
+{
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    ASSERT_GT(platforms.size(), 0) << "No platforms available";
+
+    cl::Platform default_platform = platforms[0];
+    std::string default_plat_name = default_platform.getInfo<CL_PLATFORM_NAME>();
+    std::cout << "Default Platform is: " << default_plat_name << std::endl;
+
+    std::vector<cl::Device> all_devices;
+    default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+
+    ASSERT_GT(all_devices.size(), 0) << "No devices on the platform available";
+}
+
+TEST(OpenClInfo, Devices)
+{
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    ASSERT_GT(platforms.size(), 0) << "No platforms available";
+    std::cout << "Number of Platforms " << platforms.size() << std::endl;
+    cl::Platform default_platform = platforms[0];
+    std::cout << "Default Platform is: " << default_platform.getInfo<CL_PLATFORM_NAME>()
+              << std::endl;
+
+    std::vector<cl::Device> all_cpus;
+    platforms[1].getDevices(CL_DEVICE_TYPE_CPU, &all_cpus);
+    ASSERT_GT(all_cpus.size(), 0) << "No CPUs on the platform available";
+
+    std::vector<cl::Device> all_gpus;
+    platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &all_gpus);
+    ASSERT_GT(all_gpus.size(), 0) << "No CPUs on the platform available";
+}
+
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
